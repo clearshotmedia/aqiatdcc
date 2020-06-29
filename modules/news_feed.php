7ca0f0af1095c8b7d7040ce92eddf067b9b641be
clearshotmedia <lara@clearshotmedia.com.au>
2020-06-29 14:10:33 +1000

    mobile header changed

--- modules/news_feed.php
+++ modules/news_feed.php
@@ -8,7 +8,7 @@
     <div class="container">
         <div class="row ">
         
-            <div class="col-md-6">
+            <div class="col-md-6 d-none d-md-block">
         
             <div id="newsout">    </div>
                 <img src="<?php echo $background;?>" alt="background news">
@@ -19,10 +19,10 @@
             <?php foreach( $recent_posts as $recent ){  ?>
             <a id="<?php echo $recent["ID"];?>" href="#"><div class="post-inner"  style="margin-bottom:10px;">
                 <div class="row">
-                    <div class="col-sm-2 date"><h3>07
-                    </h3><h6>JUL</h6></div>
-                    <div class="col-sm-10"><span class="news-heading"><?php echo $recent["post_title"]; ?></span>
-                    <span class="meta"><?php echo date('M Y ', strtotime($recent["post_date"])); ?></span>
+                    <div class="col-2 date"><h3><?php echo date('d', strtotime($recent["post_date"])); ?>
+                    </h3><h6><?php echo date('M', strtotime($recent["post_date"])); ?></h6></div>
+                    <div class="col-10"><span class="news-heading"><?php echo $recent["post_title"]; ?></span>
+                   <a class="link-front" href="/test"> <span class="meta">View News <?php echo get_the_category($recent["ID"]); ?></span></a>
                 </div>
                 </div></div> </a>
 
