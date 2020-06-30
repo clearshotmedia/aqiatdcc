6d53bccc1432fde7680e7cdc6c40b2453ab66c7e
clearshotmedia <lara@clearshotmedia.com.au>
2020-06-30 15:15:11 +1000

    idk

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -30,7 +30,17 @@ remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 )
 		add_filter( 'woocommerce_form_field_args', 'tdcc_wc_form_field_args', 10, 3 );
 	}
 }
+// Replace add to cart button by a linked button to the product in Shop and archives pages
+add_filter( 'woocommerce_loop_add_to_cart_link', 'replace_loop_add_to_cart_button', 10, 2 );
+function replace_loop_add_to_cart_button( $button, $product  ) {
+    // Not needed for variable products
+    if( $product->is_type( 'variable' ) ) return $button;
 
+    // Button text here
+    $button_text = __( "View product", "woocommerce" );
+
+    return '<a class="button" href="' . $product->get_permalink() . '">' . $button_text . '</a>';
+}
 /* overwrite product title */ 
 remove_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10 );
 add_action( 'woocommerce_shop_loop_item_title', 'tdcc_template_loop_product_title', 10 );
